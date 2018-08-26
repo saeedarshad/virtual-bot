c1d9b574716096af672a4b15d5c77087e5106f48
Saeed Arshad <m.saeedarshad95@gmail.com>
2018-08-27 01:03:13 +0500

    refactor

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-"use strict";
-
 const express = require("express");
 const bodyParser = require("body-parser");
+const message = require('./routes/message');
+
+require('./db')();
 
 const app = express();
 
@@ -13,22 +14,15 @@ app.use(
 
 app.use(bodyParser.json());
 
-app.post("/echo", function(req, res) {
-  var speech =
-    req.body.queryResult &&
-    req.body.queryResult.parameters &&
-    req.body.queryResult.parameters.echoText
-      ? req.body.queryResult.parameters.echoText
-      : "Seems like some problem. Speak again.";
-      
-  return res.json({
-    fulfillmentText: speech + " response from node app",
-    source: "webhook-echo-sample"
-  });
+app.get('/', (req, res) => {
+  res.send('Virtual sales bot End poin');
 });
+
+app.use('/message' , message)
+ 
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Server up and listening at" , port);
+app.listen(port, function () {
+  console.log("Server up and listening at", port);
 });
