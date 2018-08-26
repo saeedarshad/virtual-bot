e65f4734efa90dab5d4aa8af20c73f9f3cc7a96a
Saeed Arshad <m.saeedarshad95@gmail.com>
2018-08-27 01:16:38 +0500

    gg

--- routes/message.js
+++ routes/message.js
@@ -3,23 +3,17 @@ const moongoose = require('mongoose');
 
 const router = express.Router();
 
-router.get('/',(req, res) => {
+router.get('/', (req, res) => {
     res.send('Get request on end point');
 });
 
 
-router.post('/',(req, res) => {
-    var speech =
-    req.body.queryResult &&
-      req.body.queryResult.parameters &&
-      req.body.queryResult.parameters.echoText
-      ? req.body.queryResult.parameters.echoText
-      : "Seems like some problem. Speak again.";
-
-  return res.json({
-    fulfillmentText: speech + " response from node app",
-    source: "webhook-echo-sample"
-  });
+router.post('/', (req, res) => {
+    var speech = req.body.queryResult.parameters.echoText;
+    return res.send({
+        fulfillmentText: speech + " response from node app",
+        source: "webhook-echo-sample"
+    });
 });
 
 
