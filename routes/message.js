4beff02e8ac39838739b13fb68acdaa730ab7001
Saeed Arshad <m.saeedarshad95@gmail.com>
2018-10-09 00:14:42 +0500

    checking different info of request object

--- routes/message.js
+++ routes/message.js
@@ -14,7 +14,12 @@ router.post("/", async (req, res) => {
   var colour = req.body.queryResult.parameters.colour;
   var memory = req.body.queryResult.parameters.memorygb;
   var session = req.body.session;
+  var responseId = req.body.responseId;
   var speech_confidence = req.body.queryResult.speechRecognitionConfidence;
+  var action = req.body.queryResult.responseId;
+  var context = req.body.queryResult.context;
+  var intentDetectionConfidence = req.body.queryResult.intentDetectionConfidence;
+  var diagnosticInfo = req.body.queryResult.diagnosticInfo;
 
   const iphone = await Iphone.findOne({
     price: {
@@ -36,6 +41,13 @@ router.post("/", async (req, res) => {
   console.log('iphone id: ', iphone._id);
   console.log('iphone Title: ', iphone.title);
   console.log('Speech ', speech_confidence);
+  console.log('Response Id ', responseId);
+  console.log('Action ', action);
+  console.log('output context', context);
+  console.log(' intentDetectionConfidence', intentDetectionConfidence);
+  console.log(' diagnosticInfo', diagnosticInfo);
+
+
 
   /* var mobile = req.body.queryResult.parameters.mobiles;
   //var memory = req.body.queryResult.parameters.memorygb;
